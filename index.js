d2ce44e299dd50a74716adc6bfcb809f11f0f70c
DVDFE <davidfe.pro@gmail.com>
2022-03-16 14:50:23 +0100

    Scroll Event

--- index.js
+++ index.js
@@ -80,7 +80,20 @@ document.addEventListener('keypress', (e) => {
     } else {
         KeyPressContainer.style.background = "red";
     }
-
-    
 })
 
+//--------------------------------------------------------
+//Scroll Event
+
+const nav = document.querySelector("nav")
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 120){
+        nav.style.top = 0;
+    }
+    else{
+        nav.style.top = "-50px";
+    }
+})
+
+//---------------------------------------------------
